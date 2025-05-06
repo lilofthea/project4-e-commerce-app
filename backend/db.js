8cafe2897fa8ff163394dcb8d27d27da6d1c4e2d
semihcngz <scengiz2002@hotmail.com>
2025-05-07 02:46:54 +0300

    database bug fix

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
